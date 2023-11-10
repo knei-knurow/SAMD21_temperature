fb96bbe613caffb766f0f8eae4977ae1372f875b
komputer3 <121889984+MichalZywerKNEI@users.noreply.github.com>
2023-11-10 18:41:19 +0100

    Define ROM, Temperature and Memory commands

--- SAMD21_onewire.h
+++ SAMD21_onewire.h
@@ -15,6 +15,20 @@
 extern "C" {
 #endif
 
+#define ONEWIRE_SEARCH_ROM 0xF0			//ROM commands
+#define ONEWIRE_READ_ROM 0x33
+#define ONEWIRE_MATCH_ROM 0x55
+#define ONEWIRE_SKIP_ROM 0xCC
+#define ONEWIRE_ALARM_SEARCH 0xEC
+
+#define ONEWIRE_CONVERT_T 0x44			//Temperature command
+
+#define ONEWIRE_SCRATCHPAD_WRITE 0xBE	//Memory commands
+#define ONEWIRE_SCRATCHPAD_READ 0x4E
+#define ONEWIRE_SCRATCHPAD_COPY 0x48
+#define ONEWIRE_RECALL_E2 0xB8
+#define ONEWIRE_READ_POWER_SUPPLY 0xB4
+
 
 
 #ifdef __cplusplus
