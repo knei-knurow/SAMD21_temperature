a3af6333415a100aa217f7bf71f28ae7dfb1411d
MichalZywerKNEI <121889984+MichalZywerKNEI@users.noreply.github.com>
2023-11-15 10:26:06 +0100

    Try using the library (not tested)

--- main.c
+++ main.c
@@ -1,18 +1,14 @@
 #include <atmel_start.h>
-//#include "SAMD21_onewire.h" //include our Onewire library
+#include "SAMD21_onewire.h" //include our Onewire library
 
 int main(void)
 {
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
 
-	//delay_ms(500);
-	
-	//uint8_t *data;
+	delay_ms(500);
 	
 	while (1) {
-		/*gpio_toggle_pin_level(PINA1);
-		delay_ms(500);
 		Onewire_ResetPulse();
 		Onewire_DetectPresence();
 		if(Onewire_SensorPresent){
@@ -21,10 +17,16 @@ int main(void)
 			delay_ms(1000);
 			Onewire_WriteData(ONEWIRE_SKIP_ROM, 1);
 			Onewire_WriteData(ONEWIRE_SCRATCHPAD_READ, 1);
-			data = Onewire_ReadData(3);
-			
+			Onewire_ReadData(2);
+			gpio_set_pin_level(PINA0, Onewire_Data[0] & 16);
+			gpio_set_pin_level(PINA1, Onewire_Data[0] & 32);
+			gpio_set_pin_level(PINA2, Onewire_Data[0] & 64);
+			gpio_set_pin_level(PINA3, Onewire_Data[0] & 128);
+			gpio_set_pin_level(PINA4, Onewire_Data[1] & 1);
+			gpio_set_pin_level(PINA5, Onewire_Data[1] & 2);
+			gpio_set_pin_level(PINA6, Onewire_Data[1] & 4);
 		}
-		else{*/
+		else{
 			gpio_toggle_pin_level(PINA0);
 			gpio_toggle_pin_level(PINA1);
 			gpio_toggle_pin_level(PINA2);
@@ -32,7 +34,7 @@ int main(void)
 			gpio_toggle_pin_level(PINA4);
 			gpio_toggle_pin_level(PINA5);
 			gpio_toggle_pin_level(PINA6);
-			delay_ms(500);/*
-		}*/
+			delay_ms(500);
+		}
 	}
 }
